ac33393da82d244c308653a0378ae13a144329b6
gregorymartin <77732347+gregorymartin200261@users.noreply.github.com>
2022-09-13 23:15:04 +0200

    Add files via upload

---
+++ exchien/exchien/Chien.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exchien
+{
+    class Chien
+    {
+        private string _nom;
+        private string _race;
+        private uint _age;
+
+        public Chien(string nom,string race,uint age)
+        {
+            this._nom = nom;
+            this._race = race;
+            this._age = age;
+        }
+
+        public string AfficheCaracteristique()
+        {
+            return "Nom : " + this._nom + " - Age : " + this._age + " - Race " + this._race;
+        }
+    }
+
+
+}
---
+++ exchien/exchien/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exchien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue dans notre chenil");
+            Chien medor = new Chien("Médor", "Caniche", 13);
+            Chien[] mesChiens = new Chien[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("quel est le nom du chien?");
+                string nomChien = Console.ReadLine();
+                Console.WriteLine("quel est la race du chien?");
+                string raceChien = Console.ReadLine();
+                Console.WriteLine("quel est l'age du chien?");
+                uint ageChien = uint.Parse(Console.ReadLine());
+
+                mesChiens[i] = new Chien(nomChien, raceChien, ageChien);
+                Console.WriteLine(mesChiens[i].AfficheCaracteristique());
+            }
+        }
+    }
+}
